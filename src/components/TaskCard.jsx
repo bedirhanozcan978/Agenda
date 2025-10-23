e1d6066da515000372caa4087287def428637a7f
NightWolf978 <ozcanb978@gmail.com>
2025-10-23 13:56:21 +0300

    TaskCard UI Improvement

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,14 +1,28 @@
 export default function TaskCard({ task }) {
   return (
-    <div className="bg-creme rounded-xl shadow p-3 flex justify-between items-center">
-      <div>
-        <p className="">{task.title}</p>
-        <p className="text-sm text-gray-600">{task.start} / {task.end}</p>
-        <p className="text-xs text-blue-600">#{task.tag}</p>
+    <div className="bg-creme rounded-xl shadow p-3 flex flex-col justify-between items-start">
+      
+      <p className="text-xl">{task.title}</p>
+
+      <div className="w-full flex flex-row justify-between items-baseline pt-2 text-lg">
+        <div className="flex items-baseline cursor-pointer">
+          <label htmlFor={`task-${task.id}`} className="flex cursor-pointer space-x-2">
+            Done
+            <div className="relative ml-1 w-4 h-4">
+              <input type="checkbox" checked={task.done} className="appearance-none w-full h-full border-2 rounded-full border-gray-600 checked:border-green" />
+              {task.done && (<i className={`fa fa-check text-sm text-green absolute inset-0 m-auto`}></i>)}
+            </div>  
+
+          </label>
+        </div>
+        <p className="text-gray-600">{task.start} / {task.end}</p>
       </div>
-      <div>
-        <input type="checkbox" checked={task.done} className="scale-125 accent-green-600" />
+
+      <div className="w-full flex flex-row border-t border-gray-600">
+        <p className="text-sm pr-1">Tags:</p>
+        <p className="text-sm pr-1 text-blue-600">#{task.tag}</p>
       </div>
+
     </div>
   )
 }
