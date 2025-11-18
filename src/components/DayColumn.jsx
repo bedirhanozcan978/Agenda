2bc7cbf7ec266263df0d6085616bd72f0b0d1698
NightWolf978 <ozcanb978@gmail.com>
2025-11-18 17:42:15 +0300

    Progress bar added

--- src/components/DayColumn.jsx
+++ src/components/DayColumn.jsx
@@ -33,11 +33,40 @@ export default function DayColumn ({dayData}){
     };
     const nexTaskTime = getLastHour();
 
+    // Progress Bar Width
+
+    const calculateProgress = () => {
+        const totalTasks = dayData.tasks.length;
+
+        if (totalTasks === 0) {
+            return "0%"; 
+        }
+
+        const doneTasks = dayData.tasks.filter(task => task.done).length;
+
+        if (doneTasks == 0) {
+            return "0%";
+        }
+
+        const percentage = Math.round((doneTasks / totalTasks) * 100);
+
+        return `${percentage}%`;
+    };
+
+    const progressWidth = calculateProgress();
+
+
     return (
         <div className="flex-1 min-w-0">
             <div className="px-4 flex flex-row items-baseline justify-between">
                 <h2 className="text-2xl mb-2">{dayData.weekday}</h2> 
                 <h3 className="text-xl text-gray-600"> {dayData.month} {dayData.day} </h3>
+            </div>
+
+            <div className="px-4 pb-4">
+                <div className="w-full bg-gray-300 rounded-full h-3">
+                    <div className="bg-green h-3 rounded-full transition-all" style={{width: progressWidth}}></div>
+                </div>
             </div>
                 
             <div className="flex flex-col gap-2">
